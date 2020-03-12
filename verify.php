226dc14046fdddd7bdf44dd98601189c7782bc9c
NodeMCU-esp8266 <62006514+NodeMCU-esp8266@users.noreply.github.com>
2020-03-12 16:49:09 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'LINE_token_ID';    //PUT LINE token ID at "Channel access token (long-lived)"
+$access_token = 'ZsjooYaFc1A8q/6SxuUfyRR1Lm5/24rZGtMp/CjtF//WV3G00ggLmm+zM8jbOGbNnaj+o9Te50l4TumGndTTQ8k/7VgRftZAPAGZLcVhln8GRSPDwBx0XPzqXkVTi+9VSouiF8yao+tKNiy+jh71QQdB04t89/1O/w1cDnyilFU=';    //PUT LINE token ID at "Channel access token (long-lived)"
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 
